567255fdac242955a506bfb94f331244d7c5a56b
Zack <zachariekabeya@gmail.com>
2022-09-04 10:36:08 -0800

    third commit

---
+++ app/src/main/java/com/example/paintapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.paintapp;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Paint_view paint_view = new Paint_view(this);
+        setContentView(paint_view);
+    }
+}
